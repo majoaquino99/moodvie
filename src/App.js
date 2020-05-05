2388d619d787b0e1dea742885ebd4f5832dcc72c
majoaquino99 <majo.aqh@gmail.com>
2020-05-05 11:36:16 -0500

    added css root variables

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Landing from './components/landing/LandingPage';
 
 function App() {
   return (
     <div className="App">
-     
+     <Landing/>
     </div>
   );
 }
