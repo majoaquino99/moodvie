6510e26834964aacbc6447d2ac0cacbf78341ccc
jossAcosta <josselineacostamtz@gmail.com>
2020-05-07 08:37:00 -0500

    :lipstrick: responsive web landscape

--- src/App.js
+++ src/App.js
@@ -8,11 +8,6 @@ import { Result, LandingView } from './views';
 
 
 function App() {
-<<<<<<< HEAD
-  
-=======
-
->>>>>>> 226a369a50709cf32a218e29bc5694efd2df9bc7
   return (
       <Router>
         <Switch>
