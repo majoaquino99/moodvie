6117a6e29424459d7535265bd82ef4df7f402acc
majoaquino99 <majo.aqh@gmail.com>
2020-05-07 09:09:15 -0500

    Updates

---
+++ precache-manifest.5b6f83428b0db38d95d629313ad60ca3.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91adab4faf5e0bc7e08058b3fcda2e5a",
+    "url": "/moodvie/index.html"
+  },
+  {
+    "revision": "9a762fb27c8ae29f9a7e",
+    "url": "/moodvie/static/css/main.dfa17c05.chunk.css"
+  },
+  {
+    "revision": "938498ce88cd2cd52918",
+    "url": "/moodvie/static/js/2.5688c461.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/moodvie/static/js/2.5688c461.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a762fb27c8ae29f9a7e",
+    "url": "/moodvie/static/js/main.5e8b1e9b.chunk.js"
+  },
+  {
+    "revision": "8023c638031854f576c5",
+    "url": "/moodvie/static/js/runtime-main.24c482d0.js"
+  },
+  {
+    "revision": "78acabf9c426b2cb93c3dacbe97e0d6a",
+    "url": "/moodvie/static/media/action.78acabf9.svg"
+  },
+  {
+    "revision": "e11f5b266dd70d32d874eaa37775168a",
+    "url": "/moodvie/static/media/back.e11f5b26.svg"
+  },
+  {
+    "revision": "0d228ebfdbe79583ea6a7c333ae65c84",
+    "url": "/moodvie/static/media/clockLess.0d228ebf.svg"
+  },
+  {
+    "revision": "c350e5e92555b9695cf471824ddb05c3",
+    "url": "/moodvie/static/media/clockMore.c350e5e9.svg"
+  },
+  {
+    "revision": "80017469ed96ec45d0d0b28bf004fa83",
+    "url": "/moodvie/static/media/home.80017469.svg"
+  },
+  {
+    "revision": "b5a6ffda773aa193a1b3ebed568d1bd3",
+    "url": "/moodvie/static/media/logo.b5a6ffda.svg"
+  },
+  {
+    "revision": "e3a59f9dc5c8e91a1b3b087faa0b2f59",
+    "url": "/moodvie/static/media/logo.e3a59f9d.svg"
+  },
+  {
+    "revision": "46f8ec1ff2e63b0a88f95fe0c2e0ed4f",
+    "url": "/moodvie/static/media/musical.46f8ec1f.svg"
+  },
+  {
+    "revision": "bf851c6f1f98fe20a1b16238bdc11ece",
+    "url": "/moodvie/static/media/romance.bf851c6f.svg"
+  },
+  {
+    "revision": "be1f7e526465c5882437ba13d35d216d",
+    "url": "/moodvie/static/media/thriller.be1f7e52.svg"
+  }
+]);
